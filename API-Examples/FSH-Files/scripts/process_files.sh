6490a2bf62a8cb70f16006775e0456ebe1879d4d
florianschoffke <florian.schoffke@gematik.de>
2025-01-07 09:58:50 +0100

    output to json

--- API-Examples/FSH-Files/scripts/process_files.sh
+++ API-Examples/FSH-Files/scripts/process_files.sh
@@ -211,6 +211,7 @@ files_to_copy=(
     "Medication-erp-eml-epa-notes-14-ibm-medication-compounding-amount-Mapped.json"
     "Medication-erp-eml-epa-notes-16-KPGVerordnung-PZN-Mapped.json"
     "Medication-erp-eml-epa-notes-18-ibm-medication-compounding-text-Mapped.json"
+    "Medication-erp-eml-epa-notes-20-verordnung-pzn-Mapped.json"
 
     "Bundle-erp-versicherte-01-response-taskGetAll.json"
     "Bundle-erp-versicherte-02-response-taskGetSingle.json"
